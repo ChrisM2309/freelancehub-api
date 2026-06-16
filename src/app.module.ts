3efc383dc477bd68e76b7f7bf5520b3866f6d922
ChrisM2309 <chris.marroquin2309@gmail.com>
2026-06-16 16:18:31 -0600

    archivos creados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { AuthModule } from './auth/auth.module';
+import { PublicController } from './public/public.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, ServicesModule, AuthModule],
+  controllers: [AppController, PublicController],
   providers: [AppService],
 })
 export class AppModule {}
